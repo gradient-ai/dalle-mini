0ad7110e9f6b964da9b62a7050336de07e1c18d8
jameshskelton <jameshskelton@gmail.com>
2022-06-23 11:53:56 -0400

    removed requirement for wandb

--- src/dalle_mini/model/utils.py
+++ src/dalle_mini/model/utils.py
@@ -11,17 +11,20 @@ class PretrainedFromWandbMixin:
         """
         Initializes from a wandb artifact or delegates loading to the superclass.
         """
-        with tempfile.TemporaryDirectory() as tmp_dir:  # avoid multiple artifact copies
-            if ":" in pretrained_model_name_or_path and not os.path.isdir(
-                pretrained_model_name_or_path
-            ):
-                # wandb artifact
-                if wandb.run is not None:
-                    artifact = wandb.run.use_artifact(pretrained_model_name_or_path)
-                else:
-                    artifact = wandb.Api().artifact(pretrained_model_name_or_path)
-                pretrained_model_name_or_path = artifact.download(tmp_dir)
-
-            return super(PretrainedFromWandbMixin, cls).from_pretrained(
+        super(PretrainedFromWandbMixin, cls).from_pretrained(
                 pretrained_model_name_or_path, *model_args, **kwargs
             )
+        # with tempfile.TemporaryDirectory() as tmp_dir:  # avoid multiple artifact copies
+        #     if ":" in pretrained_model_name_or_path and not os.path.isdir(
+        #         pretrained_model_name_or_path
+        #     ):
+        #         # wandb artifact
+        #         if wandb.run is not None:
+        #             artifact = wandb.run.use_artifact(pretrained_model_name_or_path)
+        #         else:
+        #             artifact = wandb.Api().artifact(pretrained_model_name_or_path)
+        #         pretrained_model_name_or_path = artifact.download(tmp_dir)
+
+        #     return super(PretrainedFromWandbMixin, cls).from_pretrained(
+        #         pretrained_model_name_or_path, *model_args, **kwargs
+        #     )
